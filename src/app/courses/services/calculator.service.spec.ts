3271e9c00e0a4f58a0156f043137649a455f073b
iampratiktandel <iampratiktandel@gmail.com>
2022-06-02 23:02:49 +0530

    feat: beforeEach and createSpyObj

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -3,14 +3,21 @@ import { LoggerService } from "./logger.service"
 
 describe('CalculatorService', () => {
 
+  let calculator: CalculatorService;
+  let loggerSpy: any;
+
+  beforeEach(() => {
+    loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+    calculator = new CalculatorService(loggerSpy);
+  })
   it('should add two numbers', () => {
-    const calculator = new CalculatorService(new LoggerService());
     const result = calculator.add(2, 2);
     expect(result).toBe(4);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
   it('should subtract two numbers', () => {
-    const calculator = new CalculatorService(new LoggerService());
     const result = calculator.subtract(4, 3);
     expect(result).toBe(2, 'unexpected subtraction result');
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 })
