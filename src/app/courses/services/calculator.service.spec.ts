aab0aef0419b3cc51af98fc8163f39d5ced9bef9
iampratiktandel <iampratiktandel@gmail.com>
2022-06-02 23:15:15 +0530

    feat: configureTestingModule & DI

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,5 +1,6 @@
+import { TestBed } from "@angular/core/testing";
 import { CalculatorService } from "./calculator.service"
-import { LoggerService } from "./logger.service"
+import { LoggerService } from "./logger.service";
 
 describe('CalculatorService', () => {
 
@@ -8,7 +9,15 @@ describe('CalculatorService', () => {
 
   beforeEach(() => {
     loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
-    calculator = new CalculatorService(loggerSpy);
+
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        { provide: LoggerService, useValue: loggerSpy }
+      ]
+    });
+
+    calculator = TestBed.inject(CalculatorService);
   })
   it('should add two numbers', () => {
     const result = calculator.add(2, 2);
