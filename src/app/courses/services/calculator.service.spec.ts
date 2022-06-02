ce454767474c874507937b266a64b5a2794d71f3
iampratiktandel <iampratiktandel@gmail.com>
2022-06-02 22:44:23 +0530

    init commit

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,16 @@
+import { CalculatorService } from "./calculator.service"
+import { LoggerService } from "./logger.service"
+
+describe('CalculatorService', () => {
+
+  it('should add two numbers', () => {
+    const calculator = new CalculatorService(new LoggerService());
+    const result = calculator.add(2, 2);
+    expect(result).toBe(4);
+  });
+  it('should subtract two numbers', () => {
+    const calculator = new CalculatorService(new LoggerService());
+    const result = calculator.subtract(4, 3);
+    expect(result).toBe(2, 'unexpected subtraction result');
+  });
+})
