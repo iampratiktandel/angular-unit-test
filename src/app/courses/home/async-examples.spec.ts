6b5bc24c023f9036652c1d9af6167594c7e1c79a
iampratiktandel <iampratiktandel@gmail.com>
2022-06-07 00:24:50 +0530

    feat: async example - observables

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,4 +1,6 @@
-import { fakeAsync, flush, tick } from "@angular/core/testing";
+import { fakeAsync, flush, flushMicrotasks, tick } from "@angular/core/testing";
+import { of } from "rxjs";
+import { delay } from "rxjs/operators";
 
 fdescribe('Angular Testing Examples', () => {
 
@@ -18,9 +20,62 @@ fdescribe('Angular Testing Examples', () => {
     setTimeout(() => {
       test = true;
     }, 1000);
-    
+
     tick(1000);
     flush();
+    expect(test).toBeTruthy();
+  }));
+
+  it('Async test - plain Promise', fakeAsync(() => {
+    let test = false;
+
+    Promise.resolve().then(() => {
+      test = true;
+      return Promise.resolve();
+    })
+      .then(() => {
+        console.log('ad');
+      });
+    
+    flushMicrotasks();
+    expect(test).toBeTruthy();
+  }));
+
+  it('Async test - Promises + setTimeout()', fakeAsync(() => {
+    let counter = 0;
+
+    Promise.resolve()
+    .then(() => {
+      counter += 10;
+
+      setTimeout(() => {
+        counter += 1;
+      }, 1000);
+    });
+
+    expect(counter).toBe(0);
+    
+    flushMicrotasks();
+    expect(counter).toBe(10);
+
+    tick(500);
+    expect(counter).toBe(10);
+
+    tick(500);
+    expect(counter).toBe(11);
+
+  }))
+
+  it('Async test example - Observables', fakeAsync(() => {
+    let test = false;
+
+    const test$ = of(test).pipe(delay(1000));
+
+    test$.subscribe(() => {
+      test = true;
+    })
+
+    tick(1000);
     expect(test).toBeTruthy();
   }));
 })
